37e0297812c37beacdf780137ba9ef5de1a10b1b
Nagaranjitha Adhikari <nagaranjithareddy421@gmail.com>
2024-05-09 16:12:22 +0530

    first commit

--- EmpDeptJoins/urls.py
+++ EmpDeptJoins/urls.py
@@ -20,4 +20,5 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('innerEquijoins/',innerEquijoins,name='innerEquijoins'),
+    path('selfjoins/',selfjoins,name='selfjoins'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 
 
 def innerEquijoins(request):
+    #Joined Data Of Emp and Dept
     JDED = Emp.objects.select_related('DEPTNO').all()
 
     JDED = Emp.objects.select_related('DEPTNO').filter(DEPTNO = 30)
@@ -49,3 +50,48 @@ def innerEquijoins(request):
 
     d={'JDED':JDED}
     return render(request,'innerEquijoins.html',d)
+
+
+def selfjoins(request):
+
+    #EMJD -->Employee Manager Joining Data (table1 EMP , Table2 MGR)
+    EMJD = Emp.objects.select_related('MGR').all()
+
+    #emp comm is null
+    EMJD = Emp.objects.select_related('MGR').filter(COMM__isnull =True)
+
+    #emp comm is not null
+    EMJD = Emp.objects.select_related('MGR').filter(COMM__isnull =False)
+
+    #emp whose mgr name contains'a' in it
+    EMJD = Emp.objects.select_related('MGR').filter(MGR__ENAME__contains ='a')
+
+    # emp data whose mgr doesnt have any manager
+    EMJD = Emp.objects.select_related('MGR').filter(MGR__isnull =True)
+
+    #emp whose manager name starts with 'b' or 'c'
+    EMJD = Emp.objects.select_related('MGR').filter(Q(MGR__ENAME__startswith ='b' )|Q(MGR__ENAME__startswith ='c' )) 
+
+    #emp whose name contains 's' and mgr name contains 'a' or 'b'
+    EMJD = Emp.objects.select_related('MGR').filter(Q(ENAME__contains ='s') & Q(MGR__ENAME__contains ='a')|Q(MGR__ENAME__contains ='b'))
+
+    #emp hiredate before 1985
+    EMJD = Emp.objects.select_related('MGR').filter(HIREDATE__year__lt = 1985)
+
+    #emp hiredate after 1985 and before 1990
+    EMJD = Emp.objects.select_related('MGR').filter(HIREDATE__year__gt = 1985 ,HIREDATE__year__lt = 1990)
+
+    #emp whose mgr sal is 5000
+    EMJD = Emp.objects.select_related('MGR').filter(MGR__SAL=5000)
+
+    #emp whose mgr sal is in range 3000 - 5000
+    EMJD = Emp.objects.select_related('MGR').filter(MGR__SAL__range =(3000,5000))
+
+    #emp hiredate in month of dec
+    EMJD = Emp.objects.select_related('MGR').filter(HIREDATE__month = 12)
+    
+    #EMJD = Emp.objects.select_related('MGR').all()
+    
+
+    d={'EMJD':EMJD}
+    return render(request,'selfjoins.html',d)
